7e86254da49b9fc9fdf3c50674ea3997d3b02bb2
TrifonasMoschopoulos <trifonas.mpl9@gmail.com>
2026-07-11 03:12:39 +0300

    Remove setters from StudentResponse DTO
    StudentResponse is read-only and should not be mutable after creation

--- src/main/java/gr/techpro/absence/dto/response/StudentResponse.java
+++ src/main/java/gr/techpro/absence/dto/response/StudentResponse.java
@@ -37,21 +37,10 @@ public class StudentResponse {
     }
 
     public Long getId() { return this.id; }
-    public void setId(Long id) { this.id = id; } 
-    
     public String getFirstName(){ return this.firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    
     public String getLastName() { return this.lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-
     public String getEmail() { return this.email; }
-    public void setEmail(String email) { this.email = email;}
-
     public String getStudentNumber() { return this.studentNumber; }
-    public void setStudentNumber(String studentNumber) { this.studentNumber = studentNumber; }
-
     public LocalDate getEnrollmentDate() { return this.enrollmentDate; }
-    public void setEnrollmentDate(LocalDate enrollmentDate) { this.enrollmentDate = enrollmentDate; }
-
+    
 }
