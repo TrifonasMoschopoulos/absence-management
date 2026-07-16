634a64516ec23cc24704435ad1349e5c29dad653
TrifonasMoschopoulos <trifonas.mpl9@gmail.com>
2026-07-17 02:42:20 +0300

    feat(exceptions): add handlers for JSON parse errors and URL type mismatches

--- src/main/java/gr/techpro/absence/exception/GlobalExceptionHandler.java
+++ src/main/java/gr/techpro/absence/exception/GlobalExceptionHandler.java
@@ -5,10 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
@@ -65,7 +69,7 @@ public class GlobalExceptionHandler {
     }
 
 
-    // PATH VARIABLE AND QUERY PARAM EXCEPTION HANDLING
+    // PATH VARIABLE AND QUERY PARAM EXCEPTION HANDLING. Activated AT @RequestParam
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> handleConstraintViolation(ConstraintViolationException exception, HttpServletRequest request){
         
@@ -88,6 +92,53 @@ public class GlobalExceptionHandler {
     }
 
 
+
+    // Handle: Invalid JSON, filed type or wrong Enum value
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadable(HttpMessageNotReadableException exception, HttpServletRequest request){
+        
+        String errorMessage = "Malformed JSON request or invalid data types provided";
+        
+        Throwable cause = exception.getCause();
+
+        if (cause instanceof InvalidFormatException){
+            InvalidFormatException invalidException = (InvalidFormatException) cause;
+
+            String wrongValue = String.valueOf(invalidException.getValue());
+
+            Class<?> targetClass = invalidException.getTargetType();
+            String targetType = (targetClass != null)? targetClass.getSimpleName() : "Unknown Type";
+            
+            String specificMessage = String.format("Cannot convert '%s' to target type '%s'", wrongValue, targetType);
+
+            if (!invalidException.getPath().isEmpty()){
+                String fieldName = invalidException.getPath().getLast().getFieldName();
+                errorMessage = String.format("Invalid value for field '%s'. %s", fieldName, specificMessage);
+            }else{
+                errorMessage = specificMessage;
+            }
+        }
+
+        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST, errorMessage, request);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+
+
+    // Handle Invalid URL parameter type (PathVariable + Query Params)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException exception, HttpServletRequest request){
+        String argumentName = exception.getName();
+        String wrongValue = String.valueOf( exception.getValue() );
+        
+        Class<?> targetClass = exception.getRequiredType(); 
+        String targetType = (targetClass != null)? targetClass.getSimpleName() : "Unknown Type";
+
+        String errorMessage = String.format("Invalid value for URL parameter '%s'. Cannot convert '%s' to target type '%s'", argumentName, wrongValue, targetType);
+
+        ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST, errorMessage, request);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 
     
 }
